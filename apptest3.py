7eb56e540e105e6d8263d7a212d2912fbef2b320
Howard <g8r1fl@gmail.com>
2021-04-28 23:01:30 -0400

    added lebron all data and yearly data query to apptest3.py

--- apptest3.py
+++ apptest3.py
@@ -36,6 +36,8 @@ def connection():
 def read_all_data():
     with connection() as conn:
         c = conn.cursor()
+        
+        # Kobe stats all data
         kb_all_data = c.execute('''
             SELECT 
             ROW_NUMBER() OVER (ORDER BY strftime('%Y', [game.date]))
@@ -43,14 +45,34 @@ def read_all_data():
             FROM kobe 
             WHERE pts IS NOT NULL
             ''').fetchall()
+        # Kobe stats per year data
+        kb_yearly_data = c.execute('''
+            SELECT 
+            ROW_NUMBER() OVER (ORDER BY strftime('%Y', [game.date]))
+            row_num,strftime('%Y', [game.date]) as Year,sum(pts),sum(ast),sum(reb),sum(blk),sum(dreb),sum(stl) 
+            FROM kobe 
+            WHERE pts IS NOT NULL
+            GROUP BY Year
+            ''').fetchall()
 
+        # Lebron stats all data
         lj_all_data = c.execute('''
             SELECT 
             ROW_NUMBER() OVER (ORDER BY strftime('%Y', [game.date]))
             row_num,strftime('%Y',[game.date]) as Year,strftime('%Y-%m-%d',[game.date]) as Date,[game.visitor_team_id],[game.home_team_id],pts,ast,reb,blk,dreb,stl  
             FROM lebron 
             WHERE pts IS NOT NULL
-            ''').fetchall()    
+            ''').fetchall()   
+        # Lebron stats per year data
+        lj_yearly_data = c.execute('''
+            SELECT 
+            ROW_NUMBER() OVER (ORDER BY strftime('%Y', [game.date]))
+            row_num,strftime('%Y', [game.date]) as Year,sum(pts),sum(ast),sum(reb),sum(blk),sum(dreb),sum(stl) 
+            FROM lebron 
+            WHERE pts IS NOT NULL
+            GROUP BY Year
+            ''').fetchall()
+             
 
         # yearly stats arrays
         kb_year_index = [] #0
@@ -89,17 +111,54 @@ def read_all_data():
             kb_dreb.append(int(i[9])) #9
             kb_stl.append(int(i[10])) #10
         
+
+        # yearly stats arrays
+        lj_year_index = [] #0
+        lj_pts_yr = [] #2
+        lj_ast_yr = [] #3
+        lj_reb_yr = [] #4
+        lj_blk_yr = [] #5
+        lj_dreb_yr = [] #6
+        lj_stl_yr = [] #7
+        # individual games stats arrays
+        lj_year = [] #1
+        lj_date = [] #2
+        lj_pts = [] #5
+        lj_ast = [] #6
+        lj_reb = [] #7
+        lj_blk = [] #8
+        lj_dreb = [] #9
+        lj_stl = [] #10
+
+        for i in lj_yearly_data:
+            lj_year_index.append(int(i[0])) #0
+            lj_pts_yr.append(int(i[2])) #2
+            lj_ast_yr.append(int(i[3])) #2
+            lj_reb_yr.append(int(i[4])) #4
+            lj_blk_yr.append(int(i[5])) #5
+            lj_dreb_yr.append(int(i[6])) #6
+            lj_stl_yr.append(int(i[7])) #7
+
+        for i in lj_all_data:
+            lj_year.append(int(i[1])) #1
+            lj_date.append(i[2]) #2
+            lj_pts.append(int(i[5])) #5
+            lj_ast.append(int(i[6])) #6
+            lj_reb.append(int(i[7])) #7
+            lj_blk.append(int(i[8])) #8
+            lj_dreb.append(int(i[9])) #9
+            lj_stl.append(int(i[10])) #10
         # Kim's original code
-        data = []
-        for row in c.fetchall():
-            data.append(
-                {
-                    "name": row[0],
-                    "team_id": row[1],
-                    "player_id": row[2],
-                    "season": row[3]
-                }
-            )
+        # data = []
+        # for row in c.fetchall():
+        #     data.append(
+        #         {
+        #             "name": row[0],
+        #             "team_id": row[1],
+        #             "player_id": row[2],
+        #             "season": row[3]
+        #         }
+        #     )
 
         return data
 
