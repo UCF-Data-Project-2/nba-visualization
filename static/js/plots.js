9de51bb35bdc86d9fdda940556f53743b16d88a3
Howard <g8r1fl@gmail.com>
2021-04-29 18:06:32 -0400

    corrected minor errors in js

--- static/js/plots.js
+++ static/js/plots.js
@@ -1,7 +1,7 @@
 var trace1 = {
-    x: kb_year_index,
+    x: data.kobe.kb_year_index,
     y: data.kobe.kb_pts_yr,
-    name: "Kobe",
+    name: 'Kobe',
     mode: 'lines+markers',
     type: 'scatter'
   };
@@ -12,18 +12,13 @@ var trace2 = {
   mode: 'lines+markers',
   type: 'scatter'
   };
-// var trace3 = {
-//   x: [1, 2, 3, 4],
-//   y: [12, 9, 15, 12],
-//   mode: 'lines+markers',
-//   type: 'scatter'
-//   };
+
 var layout = {
   title: "Kobe vs Lebron - Points",
   yaxis: { title: "Points"},
   xaxis: { title: "Year Index"}
   };
   
-var data = [trace1, trace2;
+var data = [trace1, trace2];
 console.log(data.kobe.kb_pts_yr)
 Plotly.newPlot('myDiv', data);
