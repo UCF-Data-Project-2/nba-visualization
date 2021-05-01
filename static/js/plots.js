49195d7fd6508e962b5ad1cf4c7e4ddc0f97f9c9
Howard <g8r1fl@gmail.com>
2021-04-30 20:13:04 -0400

    updated plot.js

--- static/js/plots.js
+++ static/js/plots.js
@@ -1,51 +1,31 @@
-
-
-
 var trace1 = {
-<<<<<<< HEAD
-<<<<<<< HEAD
-  x: data.kobe.kb_year_index,
-  y: data.kobe.kb_pts_yr,
+  x: data.kobe.index_year,
+  y: data.kobe.pts_year,
   name: "Kobe",
   mode: 'lines+markers',
   type: 'scatter'
-=======
-    x: kb_year_index,
-    y: data.kobe.kb_pts_yr,
-    name: "Kobe",
-=======
-    x: data.kobe.index_year,
-    y: data.kobe.pts_year,
-    name: 'Kobe',
->>>>>>> 6137e27ce8cbb9b17aa64a851c4518f0570a150f
-    mode: 'lines+markers',
-    type: 'scatter'
->>>>>>> cb53a816777b55c8dd84677bbfe781927361d55e
   };
 var trace2 = {
-  x: data.lebron.index_year,
+  x: data.kobe.index_year,
   y: data.lebron.pts_year,
   name: 'Lebron',
   mode: 'lines+markers',
   type: 'scatter'
   };
-
+// var trace3 = {
+//   x: [1, 2, 3, 4],
+//   y: [12, 9, 15, 12],
+//   mode: 'lines+markers',
+//   type: 'scatter'
+//   };
 var layout = {
   title: "Kobe vs Lebron - Points",
   yaxis: { title: "Points"},
   xaxis: { title: "Year Index"}
   };
   
-var data = [trace1, trace2];
-<<<<<<< HEAD
+var chart1 = [trace1, trace2];
 
-console.log(data.kobe.kb_pts_yr)
+console.log(data)
 
-Plotly.newPlot('myDiv', data);
-=======
-Plotly.newPlot('interactive', data, layout);
-
-
-// static charts
-
->>>>>>> 6137e27ce8cbb9b17aa64a851c4518f0570a150f
+Plotly.newPlot('interactive', chart1, layout);
